54bd92685e41d44d3a7792ac7c40ebfa7b11a4b1
Garcias <sergio.garcia.sti@gmail.com>
2017-10-30 01:28:52 +0100

    Me da error en el tiempo, no suma el EXPIRE

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 import socketserver
 import sys
 import json
+import time
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
@@ -17,29 +18,42 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
         """
         Lo usamos para crear el archivo json
         """
-        json.dump(self.Client_data, open('registered.json', "w"))
+        with open('registered.json', "w") as outfile:
+            json.dump(self.Client_data, outfile, sort_keys=True, indent=4)
+    def time(self):
+
+        Actual_Time = int(time.time())
+        Actual_Time_str = time.strftime('%Y-%m-%d %H:%M:%S',
+                          time.gmtime(time.time()))
+
+
     def handle(self):
         """
         handle method of the server class
         (all requests will be handled by this method)
         """
+        atributos = {} # Value de datos del cliente.
         self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
-
         LINE = self.rfile.read()
         DATA = LINE.decode('utf-8')
         CORTES = DATA.split(' ')
+        EXPIRE = CORTES[4][:-4]
+        time_expire_str = time.strftime('%Y-%m-%d %H:%M:%S',
+                          time.gmtime(time.time()+int(EXPIRE)))
         if CORTES[0] == 'REGISTER':
-            if CORTES[4] != '0\r\n\r\n':
-                self.Client_data[CORTES[2]] = self.client_address[0]
+            if EXPIRE != '0':
+                atributos['address'] = self.client_address[0]
+                atributos['expires'] = time_expire_str
+                self.Client_data[CORTES[2]] = atributos
             else:
                 self.Client_data.pop(CORTES[2])
         print("Datos cliente(IP, puerto): " + str(self.client_address))
-        print("El cliente nos manda ", DATA)
-        print(self.Client_data)
-        print(" =============================")
+        print("El cliente nos manda ", DATA[:-4])
+       #print(self.Client_data)
+        #print(" =============================")
         self.register2json()
 if __name__ == "__main__":
-    # Listens at localhost ('') port 6001
+    # Listens at localhost ('') port sys.argv[1]
     # and calls the EchoHandler class to manage the request
     Server_port = int(sys.argv[1])
     serv = socketserver.UDPServer(('', Server_port), SIPRegisterHandler)
